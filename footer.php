c17434d82d8ebc9f08c19c1ca60239b9edc33d01
Patrick Werner <wernerp@tuta.io>
2022-04-17 09:29:46 -0400

    static frontpage complete. needs styling tweaks and value interpolation

--- footer.php
+++ footer.php
@@ -11,21 +11,39 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'xtcrbn' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'xtcrbn' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'xtcrbn' ), 'xtcrbn', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="footer bg-dark site-footer">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-sm-10 text-center brandimg"><img src="<?php echo get_template_directory_uri();?>/img/XTCrbn-Logo-Primary.png" alt="" srcset="" /></div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-lg-6 h-100 my-5 text-center">
+					<ul class="list-inline mb-0">
+						<li class="list-inline-item me-4">
+							<a href="#!"><i class="bi-facebook fs-3"></i></a>
+						</li>
+						<li class="list-inline-item me-4">
+							<a href="#!"><i class="bi-twitter fs-3"></i></a>
+						</li>
+						<li class="list-inline-item">
+							<a href="#!"><i class="bi-instagram fs-3"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="h-100 text-center text-lg-center my-auto">
+					<ul class="list-inline mb-2">
+						<li class="list-inline-item"><a href="#!">Terms &amp; Conditions</a></li>
+						<li class="list-inline-item">|</li>
+						<li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
+					</ul>
+					<p class="text-muted small mb-4 mb-lg-0">&copy; 2022 XTCRBN. All Rights Reserved.</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -15,24 +15,138 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-		<p>testing - the homepage is using this template</p>
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+	<main id="primary" class="site-main custom-background">
+		<header class="masthead">
+			<div class="container position-relative">
+				<div class="row justify-content-center">
+					<div class="col-sm-10 text-center brandimg"><img src="<?php echo get_template_directory_uri();?>/img/XTCrbn-Logo-Primary.png" alt="" srcset="" /></div>
+					<div class="col-xl-10">
+						<div class="text-center text-white">
+							<!-- Page heading-->
+							<h1 class="visually-hidden">Carbon Fiber Wakeboard towers strong light cool awesome</h1>
+							<h2 class="mb-5">Revolutionizing the tower industry</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row justify-content-center">
+					<div class="buttonContainer col-xl-2 mx-0 text-center">
+						<div class="border"></div>
+						<div class="button"><p>Get Started</p></div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<section class="text-center my-5">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-10 my-5">
+						<h2 class="orange primaryfont">About XTCrbn</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, omnis! Eligendi inventore facilis voluptatem nisi non corrupti quos laudantium neque earum sequi. Ullam praesentium exercitationem maxime ipsa laborum nobis quaerat.</p>
+						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias natus eaque asperiores expedita nemo voluptatem, tempora sunt numquam iure quisquam eligendi deleniti officiis quia eius magnam exercitationem, mollitia id nulla?</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="showcase bg-dark">
+			<div class="container-fluid p-0">
+				<div class="row justify-content-center">
+					<div class="col-lg-10 text-center">
+						<div class="row justify-content-center showcaseHeader">
+							<h2 class="lightondark primaryfont col-sm-10 col-lg-5 text-end">
+								<span class="orange">700</span>
+								% Stronger
+							</h2>
+							<h2 class="lightondark primaryfont col-sm-10 col-lg-5 text-start">
+								<span class="orange">500</span>
+								% Lighter
+							</h2>
+						</div>
+						<div class="row justify-content-center"><h5 class="lightondark primaryfont">Compared to towers on the water today</h5></div>
+					</div>
+				</div>
+				<div class="container buttonrows">
+					<div class="row justify-content-center">
+						<p class="orangeLabel lightondark text-center col-md-5 col sm-12">Improved fuel efficiency</p>
+						<p class="orangeLabel lightondark text-center col-md-5 col sm-12">Long-lasting &amp; durable</p>
+					</div>
+					<div class="row justify-content-center">
+						<p class="orangeLabel lightondark text-center col-md-5 col sm-12">Increased passenger count</p>
+						<p class="orangeLabel lightondark text-center col-md-5 col sm-12">Fewer quality issues</p>
+					</div>
+					<div class="row justify-content-center">
+						<p class="orangeLabel lightondark text-center col-md-5 col sm-12">Easier installation</p>
+						<p class="orangeLabel lightondark text-center col-md-5 col sm-12">No unsightly welds</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="aboutbrand text-center bg-light">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="patrickLogo"><img src="https://clipground.com/images/placeholder-logo-5.png" alt="" srcset="" /></div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sunt quae nihil odit. Deleniti impedit accusamus laudantium vero ipsam cupiditate aliquid eveniet beatae fuga expedita architecto, eligendi tempore unde consequatur!</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, voluptatibus voluptates alias enim facilis sit sint quibusdam iure eius consequuntur maxime veritatis adipisci doloribus, expedita aspernatur consectetur similique quas quos?</p>
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid">
+				<div class="row justify-content-center">
+					<div class="smLogo col-md-2 col-sm-6"><img src="https://clipground.com/images/placeholder-logo-5.png" alt="" srcset="" /></div>
+					<div class="smLogo col-md-2 col-sm-6"><img src="https://clipground.com/images/placeholder-logo-5.png" alt="" srcset="" /></div>
+					<div class="smLogo col-md-2 col-sm-6"><img src="https://clipground.com/images/placeholder-logo-5.png" alt="" srcset="" /></div>
+					<div class="smLogo col-md-2 col-sm-6"><img src="https://clipground.com/images/placeholder-logo-5.png" alt="" srcset="" /></div>
+					<div class="smLogo col-md-2 col-sm-6"><img src="https://clipground.com/images/placeholder-logo-5.png" alt="" srcset="" /></div>
+				</div>
+			</div>
+		</section>
+		<section class="jumboImage"></section>
+		<!-- Call to Action-->
+		<section class="call-to-action text-center">
+			<div class="container position-relative">
+				<div class="row">
+					<div class="col-xl-5 col-sm-12 beforeMessage text-start">
+						<h2 class="mb-4 orange primaryfont">Start a conversation</h2>
+						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae, quasi! Eos quos nulla odit nesciunt eligendi atque doloribus tenetur labore, fugiat esse illum amet aliquam iste possimus! Itaque, nobis laudantium.</p>
+						<ul class="list mb-0 social">
+							<li class="list-item me-4"><p>Other ways to get in touch</p></li>
+							<li class="list-item me-4">
+								<a href="#!"><i class="bi-telephone-fill fs-4">&nbsp;(XXX) XXX-XXXX</i></a>
+							</li>
+							<li class="list-item me-4">
+								<a href="#!"><i class="bi-mailbox2 fs-4">&nbsp;email@example.com</i></a>
+							</li>
+							<li class="list-item">
+								<a href="#!"><i class="bi-map fs-4">&nbsp;123 Example Road, Exampleville, GA 12345</i></a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-xl-6 col-sm-12 messageForm">
+						<!-- Email address input-->
+						<div class="row formrow"><input type="text" placeholder="Name" /></div>
+						<div class="row formrow"><input type="email" placeholder="Email" /></div>
+						<div class="row formrow"><input type="text" placeholder="Subject" /></div>
+						<div class="row formrow"><textarea placeholder="Message"></textarea></div>
+						<div class="row formrow"><button class="btn btn-orange btn-lg primaryfont" id="submitButton" type="submit">Submit</button></div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- jobs-->
+		<section class="finalcontent bg-light text-center">
+			<div class="container position-relative">
+				<div class="row justify-content-center">
+					<div class="col-xl-6">
+						<h2 class="mb-4 primaryfont orange">Join the team</h2>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea possimus obcaecati quia aspernatur est ipsa accusamus eveniet quos enim autem modi, eos quam mollitia provident tempora quas culpa voluptatem dicta.</p>
+						<buttonbtn class="btn-dark btn-lg primaryfont">Browse Jobs</buttonbtn>
+					</div>
+				</div>
+			</div>
+		</section>
+</main>
 
 <?php
 get_footer();
